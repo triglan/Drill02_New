98184941e105d295b9323e2d65012c0416c76284
triglan <glanb4277@gmail.com>
2023-09-21 12:30:50 +0900

    위엣 줄 만듦

--- character_grass.py
+++ character_grass.py
@@ -31,7 +31,10 @@ def run_rectangle():
     # bottom line
     for x in range(50, 750+1, 5):
         render_frame(x,90) #x,y 위치에 캐릭터 그려줄 수 있는
-
+        
+    # top line
+    for x in range(750, 50-1, -5):
+        render_frame(x,550)
     
 
 
