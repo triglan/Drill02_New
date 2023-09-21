7bc6adec4543efbdb6aba051f325d1dbff6fb65e
triglan <glanb4277@gmail.com>
2023-09-21 11:52:09 +0900

    원사각형 무한 루프 틀 완성

--- character_grass.py
+++ character_grass.py
@@ -10,5 +10,19 @@ grass.draw_now(400, 30)
 character.draw_now(400, 90)
 delay(5)
 
+def run_circle():
+    print('Circle')
+    pass
+
+def run_rectangle():
+    print('Rectangle')
+    pass
+    
+
+
+while True:
+    run_circle()
+    run_rectangle()
+
 close_canvas()
 
