c5d01b2699648f751a99304ba7beee3ea4973e3a
triglan <glanb4277@gmail.com>
2023-09-21 11:42:31 +0900

    소년출력완료

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,13 @@ from pico2d import *
 
 open_canvas()
 
+character = load_image('character.png')
+grass = load_image('grass.png')
+
+clear_canvas_now()
+grass.draw_now(400, 30)
+character.draw_now(400, 90)
+delay(5)
 
 close_canvas()
 
